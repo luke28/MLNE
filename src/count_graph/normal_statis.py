0e9f657a7146d267c880e040291039795592e077
luke28 <dulun2834@126.com>
2018-07-05 13:57:48 +0000

    add count_graph and split_graph(unfinished)

---
+++ src/count_graph/normal_statis.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+@ct.module_decorator
+def count_graph(params, info, **kwargs):
+    res = {}
+    res["network_path"] = info["network_path"]
+    lst = np.zeros((info["num_nodes"], ), dtype = int)
+    with open(info["network_path"], "r") as f:
+        for line in f:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            items = line.split()
+            if len(items) != 2:
+                continue
+            lst[int(items[0])] += 1
+            lst[int(items[1])] += 1
+    res["degree_distrib"] = lst
+    return res
