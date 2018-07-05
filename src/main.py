ddd627b5bceb9e14c9977f72fa105b106d3ebe0d
luke28 <dulun2834@126.com>
2018-07-05 09:29:27 +0000

    construct the structure

--- src/main.py
+++ src/main.py
@@ -7,10 +7,42 @@ import networkx as nx
 import random
 from operator import itemgetter
 
+from utils.env import *
+#from utils.datahandler import DataHandler as dh
+from utils import common_tools as ct
 
+
+def init(args, params, whole_params):
+    info = {}
+    info["time"] = ct.get_time_str()
+    info["whole_params"] = whole_params
+    info["conf_name"] = args.conf
+    info["res_home"] = os.path.join(os.path.join(RES_PATH, args.conf), info["time"])
+    # if not exists, then mkdir
+    ct.mkdir(info["res_home"])
+
+    log_path = os.path.join(LOG_PATH, info["time"] + ".log")
+    info["log"] = ct.get_logger(log_path)
+    ct.symlink(log_path, os.path.join(LOG_PATH, "new_log"))
+    ct.symlink(info["res_home"], os.path.join(RES_PATH, "new_res"))
+    return info
 
 def main():
     parser = argparse.ArgumentParser(formatter_class = argparse.RawTextHelpFormatter)
     parser.add_argument('--conf', type = str, default = "test")
     args = parser.parse_args()
-    static_info = init(params["static_info"])
+    params = ct.load_json_file(os.path.join(CONF_PATH, args.conf + ".json"))
+    info = init(args, params["static_info"], params)
+
+    res = {}
+    for module in params["run_modules"]:
+        mdl_name = module["func"]
+        mdl_params = module["params"]
+        mdl = __import__(mdl_name + '.' + mdl_params["func"], fromlist = [mdl_name])
+        print("Start %s ..." % (mdl_name, ))
+        res[mdl_name] = getattr(mdl, mdl_name)(mdl_params, info = info, res = res, mdl_name = mdl_name)
+        print res[mdl_name]
+        print("Finished!")
+
+if __name__ == "__main__":
+    main()
---
+++ src/split_graph/normal_unin_find.py
@@ -0,0 +1,7 @@
+from utils import common_tools as ct
+
+
+@ct.module_decorator
+def split_graph(params, info, **kwargs):
+    
+    return 1
