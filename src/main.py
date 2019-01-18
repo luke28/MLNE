abaa0c55de3648169f843590b939872d17acd97b
Lun Du <dulun2834@126.com>
2019-01-18 09:36:21 +0800

    copy log file to res

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import argparse
 import numpy as np
 import networkx as nx
 import random
+import shutil
 from operator import itemgetter
 
 from utils.env import *
@@ -28,6 +29,7 @@ def init(args, params, whole_params):
     ct.mkdir(info["res_home"])
 
     log_path = os.path.join(LOG_PATH, info["time"] + ".log")
+    info["log_path"] = log_path
     info["log"] = ct.get_logger(log_path)
     ct.symlink(log_path, os.path.join(LOG_PATH, "new_log"))
     ct.symlink(info["res_home"], os.path.join(RES_PATH, "new_res"))
@@ -50,6 +52,7 @@ def main():
         mdl = __import__(mdl_name + '.' + mdl_params["func"], fromlist = [mdl_name])
         res[mdl_name] = getattr(mdl, mdl_name)(mdl_params, info = info, pre_res = res, mdl_name = mdl_name)
         #print res
+    shutil.copy(info["log_path"],  info["res_home"])
 
 if __name__ == "__main__":
     main()
